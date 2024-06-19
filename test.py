32e44768bb064b42abf9529482cd54d15be93a4b
Vuong Ho <hochivuong2002@gmail.com>
2024-06-19 12:45:45 -0400

    Init flask project

---
+++ test.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request
+import requests
+
+import settings
+from openai import OpenAI
+
+app = Flask(__name__)
+
+client = OpenAI(api_key=settings.openapi_key)
+
+model = "gpt-3.5-turbo"
+
+@app.route("/")
+def index():
+    return jsonify("Hello world")
+
+@app.route("/generate_itinerary", methods=['GET'])
+def generate_itinerary():
+    user_prompt = request.args.get("prompt")
+    
+    system_prompt_file = open("./prompts/one_day_prompt_system_json.txt", "r")
+    system_prompt = system_prompt_file.read()
+
+    completion = client.chat.completions.create(
+        model = model,
+        response_format={"type": "json_object"},
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+    )
+
+    return completion.choices[0].message.content
+
+def save_to_database():
+    # TODO: save the generated prompt in a database
+    return ""
+
+if __name__ == "__main__":
+    app.run(port=8000)

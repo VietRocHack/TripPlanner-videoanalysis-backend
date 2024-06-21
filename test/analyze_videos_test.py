f3135484aae559471737590da24d709659ef4f4b
Vuong Ho <hochivuong2002@gmail.com>
2024-06-20 21:44:36 -0400

    Rename analyze videos test

--- test/analyze_videos_test.py
+++ test/analyze_videos_test.py
@@ -4,24 +4,24 @@ from function import analyze_videos
 class AnalyzeVideoUnitTest(unittest.TestCase):
 	def test_analyze_video(self):
 		video = "test/data/test_video.mp4"
-		result, error_msg = analyze_videos.analyze(video)
+		result, content = analyze_videos.analyze(video)
 
-		assert result == True
-		assert error_msg == ""
+		self.assertEquals(result, True)
+		self.assertGreater(len(content), 0)
 
 	def test_analyze_video_dne(self):
 		video = "test/data/dne.mp4"
-		result, error_msg = analyze_videos.analyze(video)
+		result, content = analyze_videos.analyze(video)
 
-		assert result == False
-		assert error_msg == "Failed to load video"
+		self.assertEquals(result, False)
+		self.assertEquals(content, "Failed to load video")
 
 	def test_sample_images(self):
 		video = "test/data/test_video.mp4"
 		num_frames_to_sample = 10
 		result = analyze_videos.sample_images(video, num_frames_to_sample)
 
-		assert len(result) == num_frames_to_sample
+		self.assertEquals(len(result), num_frames_to_sample)
 
 if __name__ == '__main__':
 	unittest.main()

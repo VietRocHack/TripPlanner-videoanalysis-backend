bfebe2c6883811c1b38f3ed88ce3652fd4a8abc0
Vuong Ho <hochivuong2002@gmail.com>
2024-06-20 21:35:55 -0400

    Finish sampling frames

---
+++ function/analyze_videos.py
@@ -0,0 +1,42 @@
+import cv2
+
+def analyze(video_url):
+	frames = []
+	try:
+		frames = sample_images(video_url)
+	except Exception as e:
+		return (False, str(e))
+
+	return (True, "")
+
+def sample_images(video_url: str, num_frames_to_sample: int = 5) -> list:
+	# Load the video
+	cap = cv2.VideoCapture(video_url)
+
+	# Check if video is successfully loaded
+	if not cap.isOpened():
+		raise Exception("Failed to load video")
+
+	frames = []
+	total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+	# Check if the number of frames in the video is greater than the desired
+	# number of frames to sample
+	num_frames_to_sample = min(num_frames_to_sample, total_frames)
+
+	# Calculate the step size to sample frames equally
+	step_size = total_frames // num_frames_to_sample
+
+	# Sample frames from the video
+	for i in range(num_frames_to_sample):
+		# Set the frame position to the desired step
+		cap.set(cv2.CAP_PROP_POS_FRAMES, i * step_size)
+		ret, frame = cap.read()
+		if not ret:
+			break
+
+		frames.append(frame)
+
+	cap.release()
+
+	return frames
--- test/function_test.py
+++ test/function_test.py
@@ -4,9 +4,24 @@ from function import analyze_videos
 class AnalyzeVideoUnitTest(unittest.TestCase):
 	def test_analyze_video(self):
 		video = "test/data/test_video.mp4"
-		result = analyze_videos.analyze(video)
+		result, error_msg = analyze_videos.analyze(video)
 
-		assert(result, True)
+		assert result == True
+		assert error_msg == ""
+
+	def test_analyze_video_dne(self):
+		video = "test/data/dne.mp4"
+		result, error_msg = analyze_videos.analyze(video)
+
+		assert result == False
+		assert error_msg == "Failed to load video"
+
+	def test_sample_images(self):
+		video = "test/data/test_video.mp4"
+		num_frames_to_sample = 10
+		result = analyze_videos.sample_images(video, num_frames_to_sample)
+
+		assert len(result) == num_frames_to_sample
 
 if __name__ == '__main__':
 	unittest.main()

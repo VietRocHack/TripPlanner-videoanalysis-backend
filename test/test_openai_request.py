c359d6739e2e470817741cb247fe3bf9c0ef044e
Vuong Ho <hochivuong2002@gmail.com>
2024-06-28 19:47:15 -0400

    FIxed openai request test

--- test/test_openai_request.py
+++ test/test_openai_request.py
@@ -1,13 +1,18 @@
 import unittest
 from function import openai_request
 import cv2
+from aiohttp import ClientSession
 
-class OpenAIRequestUnitTest(unittest.TestCase):
-	def test_send_request_image(self):
+class OpenAIRequestUnitTest(unittest.IsolatedAsyncioTestCase):
+	async def _analyze_images(self, images: list):
+		async with ClientSession() as session:
+			return await openai_request.analyze_images(session, images)
+
+	async def test_send_request_image(self):
 		# Read the image using OpenCV
 		image = cv2.imread("test/data/test_images/frame_0.jpg")
 		# Put image in list because this function takes in a list of images
-		analysis = openai_request.analyze_images([image])
+		analysis = await self._analyze_images([image])
 
 
 		self.assertIsNotNone(analysis)
@@ -16,13 +21,13 @@ class OpenAIRequestUnitTest(unittest.TestCase):
 		self.assertTrue("sandwich" in content)
 		self.assertTrue("video" in content)
 
-	def test_send_request_multiple_images(self):
+	async def test_send_request_multiple_images(self):
 		image_list = []
 		for i in range(10):
 			image_path = f"test/data/test_images/frame_{i}.jpg"
 			image = cv2.imread(image_path)
 			image_list.append(image)
-		analysis = openai_request.analyze_images(image_list)
+		analysis = await self._analyze_images(image_list)
 
 		self.assertIsNotNone(analysis)
 		self.assertIn("content", analysis)

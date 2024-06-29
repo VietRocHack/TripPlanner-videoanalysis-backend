8ba9c2b00b6b3a1f70ca7a96c44d7e21882bdde6
Vuong Ho <hochivuong2002@gmail.com>
2024-06-29 18:10:30 -0400

    Fix up some logging

--- function/utils.py
+++ function/utils.py
@@ -1,6 +1,17 @@
 import logging
+from logging.handlers import RotatingFileHandler
+import os
 
-def setup_logger(name, log_file, level=logging.DEBUG):
+def setup_logger(name, filename, level=logging.DEBUG):
+	# Ensure the logs directory exists
+	log_dir = 'logs'
+	if not os.path.exists(log_dir):
+			os.makedirs(log_dir)
+
+	# Set up the rotating file handler
+	log_file = os.path.join(log_dir, filename)
+	print(f"{name} log file is at { os.path.abspath(log_file)}")
+
 	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 	handler = logging.FileHandler(log_file, mode="a", encoding="utf-8")        
 	handler.setFormatter(formatter)

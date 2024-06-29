3c644503a23e6cf5d7178595d43ffdc297ddfd69
Vuong Ho <hochivuong2002@gmail.com>
2024-06-29 18:21:48 -0400

    docs: Fix up logging and add some documentations (#19)
    * Fix up some app stuff
    
    * Fix up some logging
    
    * Add docs for analyze_videos
    
    * Add docs to openai_request

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ load_dotenv()
 app = Flask(__name__)
 
 client = OpenAI()
-
 model = "gpt-3.5-turbo"
 
 @app.route("/")
@@ -21,9 +20,12 @@ def index():
 @app.route("/analyze_videos", methods=['POST'])
 def generate_itinerary():
 	if not request.is_json:
-		return jsonify({"error": "Bad request "}), 400
+		return jsonify({"error": "Bad request"}), 400
 
 	json_data: dict = request.get_json()
+	if "video_urls" not in json_data:
+		return jsonify({"error": "Bad request"}), 400
+		
 	# getting data for request
 	urls = json_data.get("video_urls")
 	num_frames_to_sample = json_data.get("num_frames_to_sample", 5)
